172a73e3aff0a289ab13b662c6f4d4df9db357e8
Jesse Langley <jlangleyiii@aol.com>
2020-04-12 22:57:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,11 @@
 def  reverse_each_word(sentence)
   new_sentence = sentence.split
   new_sentence.each do |word| 
-    word.collect do |letter|
-      letter.each[0] 
+    word.collect do |word|
+      end_letter = word.length 
+      while end_letter > 0 
+      end_letter -= 1 
+      
     
   end
   
