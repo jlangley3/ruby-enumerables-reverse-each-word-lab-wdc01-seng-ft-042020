ea93da1b300cc8ae3755ad67d8ce5dadd1c64b4c
Jesse Langley <jlangleyiii@aol.com>
2020-04-13 01:17:11 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,20 @@
+def  reverse_each_word(sentence)
+ new_sentence = []
+words = sentence.split
+ 
+
+     words.collect do |word| 
+    last_letter = word.length
+		new_string = ""
+		while last_letter > 0
+			last_letter -= 1
+			new_string += word[last_letter]
+		end
+		new_sentence << new_string
+		#new_sentence += (new_string)
+  new_sentence.join(" ")
+end
+final_sentence = new_sentence.join(" ")
+
+final_sentence
+end
