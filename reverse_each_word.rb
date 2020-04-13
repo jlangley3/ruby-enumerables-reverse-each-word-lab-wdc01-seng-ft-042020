bc3dba312ca023c671b1b6c5a14cf555af039082
Jesse Langley <jlangleyiii@aol.com>
2020-04-13 00:44:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,16 @@
 def  reverse_each_word(sentence)
  
 words = sentence.split
-  new_sentence = ""
+  new_sentence = " "
 
      words.collect do |word| 
     last_letter = word.length
 		new_string = ''
 		while last_letter > 0
 			last_letter -= 1
-			new_string += word[last_letter]#Reversing every letter of each word
+			new_string += word[last_letter]
 		end
-		new_sentence += (new_string + " " )
+		new_sentence += (new_string)
   
 end
 new_sentence.split(" ")
